fdabd27b7cd3a22b3addd8229a8be8789cd7cb33
David Persson <peda16@nti.umea>
2017-09-18 16:51:50 +0200

    Jobbade och utförde uppgift 2.9

--- uppgift9.java
+++ uppgift9.java
@@ -1,15 +1,26 @@
 import javax.swing.*;
-
 /*	Uppgift 2.9
 	David Persson
 	2017-09-18
 */
-	public class uppgift9{
-		public static void main(String[]args){
-			a = " Erik Andersson 860314-2714";
-			a = a.trim(); //a får värdet Erik Andersson 860314-2714
-			int i = a.lastIndexof(' ') + 1; 	// i får värdet 15
-			int j = a.indexof('-');		// b får värdet 860314
-			b = a.substring 
-		}
+
+public class uppgift9{
+	public static void main(String[]args){
+		String a = " Erik Andersson 860314-2714  ";
+/*	
+	a = a.trim();// får värdet Erik Andersson 860314-2714
+	int i = a.lastIndexOf(' ') + 1; // 14 + 1
+	int j = a.indexOf('-'); // 21
+	String b = a.substring(i, j); // värdet "860314"
+	JOptionPane.showMessageDialog(null, b);
+*/	
+	a = a.trim();// får värdet Erik Andersson 860314-2714
+	int i = a.lastIndexOf(' ') + 1; // 14 + 1
+	int j = a.indexOf('-'); 
+	String b = a.substring(19, 21); 
+	String c = a.substring(17, 19);
+	String d = b + "/" + c;
+	JOptionPane.showMessageDialog(null, d);
+
 	}
+}
