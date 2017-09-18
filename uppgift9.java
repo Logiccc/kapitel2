ee7836e9c2678fca047d2d08e1cfeca8c81f0755
David Persson <peda16@nti.umea>
2017-09-18 11:20:35 +0200

    Jobbar med 2.9

---
+++ uppgift9.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/*	Uppgift 2.9
+	David Persson
+	2017-09-18
+*/
+	public class uppgift9{
+		public static void main(String[]args){
+			a = " Erik Andersson 860314-2714";
+			a = a.trim(); //a får värdet Erik Andersson 860314-2714
+			int i = a.lastIndexof(' ') + 1; 	// i får värdet 15
+			int j = a.indexof('-');		// b får värdet 860314
+			b = a.substring 
+		}
+	}
