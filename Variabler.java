876a423e1ad5a6e0f5b4f000f19ea6b11a64423c
David Persson <peda16@nti.umea>
2017-09-18 08:58:24 +0200

    test

--- Variabler.java
+++ Variabler.java
@@ -3,6 +3,15 @@ import javax.swing.*;
 
 public class Variabler{
 	public static void main(String[]args){
+		String s1;
+		s1 = "men jaha,";
+		String s2;
+		s2 = " maybe consider";
+		String s3;
+		s3 = " kys?";
+		String s4 = "\n";
+		JOptionPane.showMessageDialog(null, s1 + s4 + s2 + s4 + s3);
+
 
 
 
